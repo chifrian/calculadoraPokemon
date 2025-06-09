b72b212776bab5aa0bf217049eb158be73d75e61
chifrian <chifrianfigueroa@gmail.com>
2025-06-09 19:55:40 -0400

    pokemon_1_0_1
    Version 1.0.1
    Se agrega la funcion suma

--- github.py
+++ github.py
@@ -1,8 +1,23 @@
 while True:
     for i in range(10):
         print(i)
-    opcion=input("Desea volver a imprimir? (si/no)")
+    opcion=input("Desea volver a imprimir? (si/no) ")
     if opcion == "si":
+        print("Let's go")
         continue
     elif opcion == "no":
+        print("Adios")
+        break
+
+print("----------------------------------------")
+
+while True:
+    for i in range(10):
+        print(i)
+    opcion=input("Desea volver a imprimir? (si/no) ")
+    if opcion == "si":
+        print("Let's go")
+        continue
+    elif opcion == "no":
+        print("Adios")
         break
